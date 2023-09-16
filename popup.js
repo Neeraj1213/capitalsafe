bd7bec3d520f0131e0c23c5f329df1590caff49a
Neeraj <neerajpillai2004@gmail.com>
2023-09-16 05:21:47 -0400

    added https portocol + Domain age API endpoint

--- popup.js
+++ popup.js
@@ -1,10 +1,43 @@
+function hasSecureProtocol(url) {
+    return url.startsWith("https://");
+}
+
+function getDomainAge(url, callback) {
+    let domain = new URL(url).hostname;
+    let apiUrl = `https://www.whoisxmlapi.com/whoisserver/WhoisService?apiKey=at_WXMdj3wIZbMsQ2pGpaIEJyINbGzpw&domainName=${domain}&outputFormat=JSON`;
+
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => {
+            let registrationDate = new Date(data.WhoisRecord.createdDate);
+            let currentDate = new Date();
+            let age = currentDate.getFullYear() - registrationDate.getFullYear();
+            callback(age);
+        })
+        .catch(error => {
+            console.error("Error fetching WHOIS data:", error);
+            callback(null);
+        });
+}
+
 chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
     let currentTab = tabs[0];
     let url = currentTab.url;
-  
-    // update div
-    document.getElementById("results").innerText = "Evaluating...";
-  
-    // URL chekc krne ke liye
-  });
-  
+
+    let resultsDiv = document.getElementById("results");
+
+    if (!hasSecureProtocol(url)) {
+        resultsDiv.innerText = "The site doesn't use a secure protocol.";
+        return;
+    }
+
+    getDomainAge(url, function(age) {
+        if (age !== null) {
+            resultsDiv.innerText = `Domain Age: ${age} years\n`;
+        } else {
+            resultsDiv.innerText = "Could not determine domain age.\n";
+        }
+        
+        resultsDiv.innerText += "Blacklist checking feature is currently disabled.";
+    });
+});
